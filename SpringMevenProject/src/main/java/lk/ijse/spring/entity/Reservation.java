141f04c330b8d457f23fad12e5b0364a1f0be4f6
Mish880 <Pereraittec6@gmail.com>
2022-07-20 15:42:24 +0530

    Update Transction

--- SpringMevenProject/src/main/java/lk/ijse/spring/entity/Reservation.java
+++ SpringMevenProject/src/main/java/lk/ijse/spring/entity/Reservation.java
@@ -15,40 +15,39 @@ import java.time.LocalDate;
 @Entity
 @ToString
 @IdClass(Orderdriver_PK.class)
+
+
+
 public class Reservation {
 
     @Id
     private String resid;
-    @Id
     private String Did;
-    @Id
     private String vid;
-    @Id
     private String Gueid;
-    @Id
     private String Regid;
-
     private LocalDate date;
     private int noofdays;
     private String pickupdate;
-   //Out=verse in driver
-   @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
-   @JoinColumn(name = "driverid",referencedColumnName = "Did",insertable = false,updatable = false)
-   private Driver driver;
+    //Out=verse in driver
+    @ManyToOne
+    @JoinColumn(name = "driverid", referencedColumnName = "Did", insertable = false, updatable = false)
+    private Driver driver;
 
-   //Out=verse in vehicle
-   @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
-   @JoinColumn(name = "vehicleid",referencedColumnName = "vid",insertable = false,updatable = false)
-   private Vehicle vehicle;
+    //Out=verse in vehicle
+    @ManyToOne
+    @JoinColumn(name = "vehicleid", referencedColumnName = "vid", insertable = false, updatable = false)
+    private Vehicle vehicle;
 
-   //Out=verse in guestcustomer
-    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
-    @JoinColumn(name = "Guestid",referencedColumnName = "Gueid",insertable = false,updatable = false)
+    //Out=verse in guestcustomer
+    @ManyToOne
+    @JoinColumn(name = "Guestid", referencedColumnName = "Gueid", insertable = false, updatable = false)
     private GuestCustomer guestCustomer;
 
     //Out=verse in regcustomer
-    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
-    @JoinColumn(name = "registrationid",referencedColumnName = "regid",insertable = false,updatable = false)
+    @ManyToOne
+    @JoinColumn(name = "registrationid", referencedColumnName = "regid", insertable = false, updatable = false)
     private RegCustomer regCustomer;
+
 
 }
