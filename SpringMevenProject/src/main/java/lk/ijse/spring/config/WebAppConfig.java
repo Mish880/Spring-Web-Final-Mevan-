cf1d220afded09063ecb0a76b9622587a34b5c26
Mish880 <Pereraittec6@gmail.com>
2022-07-26 15:18:59 +0530

    Update Error

--- SpringMevenProject/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ SpringMevenProject/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -4,32 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse")
 @EnableWebMvc
-public class WebAppConfig implements WebMvcConfigurer {
-       @Bean
-    public ModelMapper modelMapper(){
-
-           return new ModelMapper();
-       }
+public class WebAppConfig {
     @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver v= new InternalResourceViewResolver();
-        v.setPrefix("/");
-        v.setSuffix(".html");// /index.html
-        v.setOrder(2);
-        return v;
+    public ModelMapper modelMapper() {
+
+        return new ModelMapper();
     }
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/");
-    }
+
 }
 
